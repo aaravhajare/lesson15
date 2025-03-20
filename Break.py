2bf1ed3d5eb2523812286bd179fe72e8ad99330b
aaravhajare <hajareaarav66@gmail.com>
2025-03-20 11:14:43 +0530

    Break

---
+++ Break.py
@@ -0,0 +1,12 @@
+
+i = input("enter a word")
+a = input("enter a alphabet")
+
+
+for b in i :
+    if (b == a) :
+        print(a,"is found")
+        break
+
+    else:
+        print(a , "is not found")
